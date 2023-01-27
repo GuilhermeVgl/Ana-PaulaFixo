83fbd07d960ed53a894eabc063882957822e25f5
Guilherme Vieira <83255622+GuilhermeVgl@users.noreply.github.com>
2023-01-27 10:14:16 -0400

    Upload 27/01

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-// ResumeTemplate v1.0 ClearCareer
-// url: clearcareer.ca
-// by: Iskender Piyale-Sheard
-// website: izzydoesizzy.com
-// Created 06-27-2016
- 
-
-// Smooth Scroll Function
+// Função Scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+      let target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
@@ -20,5 +13,3 @@ $(function() {
     }
   });
 });
-
-
